a4b7d8297a2f780c44eed25b98beee0fb3dab560
Athlon007 <aathlon@outlook.com>
2021-02-18 19:09:44 +0100

    Initial commit

---
+++ UpdateTest/UpdateTest.cs
@@ -0,0 +1,14 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace UpdateTest
+{
+    public class UpdateTest : Mod
+    {
+        public override string ID => "UpdateTest"; //Your mod ID (unique)
+        public override string Name => "UpdateTest"; //You mod name
+        public override string Author => "Hello :)"; //Your Username
+        public override string Version => "1.0"; //Version
+        public override string UpdateLink => "https://github.com/Athlon007/UpdateTest";
+    }
+}
